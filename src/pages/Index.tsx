5273de91728073194cb470ba3727766d022c45fe
Darshan Sheta <168290335+Darshan-Sheta@users.noreply.github.com>
2026-05-08 11:41:01 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -11,7 +11,6 @@ import SectionDivider from '@/components/SectionDivider';
 const Index = () => {
   return (
     <div className="min-h-screen bg-background">
-      <PageLoader />
       <CustomCursor />
       <ScrollProgress />
       <Navbar />
